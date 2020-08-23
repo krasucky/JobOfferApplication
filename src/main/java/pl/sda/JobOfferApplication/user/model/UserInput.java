9987b72ef775496381655b103c4edf23954f1910
Patryk Krasucki <patryk95krasucki@gmail.com>
2020-08-23 10:10:37 +0200

    Poprawki, rozpoczynamy pracowac od tego samego punktu

--- src/main/java/pl/sda/JobOfferApplication/user/model/UserInput.java
+++ src/main/java/pl/sda/JobOfferApplication/user/model/UserInput.java
@@ -1,27 +1,24 @@
 package pl.sda.JobOfferApplication.user.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
 @ToString
+@Builder
 public class UserInput {
 
     private String name;
     private String login;
-    private LocalDate creationDate;
+    @JsonIgnore
+    private final LocalDate creationDate = LocalDate.now();
     private String password;
-
-    private UserInput() {
-        creationDate = LocalDate.now();
-    }
-
 
     public UserInput(String name, String login, String password) {
         this.name = name;
         this.login = login;
-        this.creationDate = LocalDate.now();
         this.password = password;
     }
 
--- src/test/java/pl/sda/JobOfferApplication/user/service/UserServiceImplTest.java
+++ src/test/java/pl/sda/JobOfferApplication/user/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package pl.sda.JobOfferApplication.user.service;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,10 +25,10 @@ class UserServiceImplTest {
     @Autowired
     UserRepository userRepository;
 
-    @AfterEach
-    void tearDown() {
-        userRepository.deleteAll();
-    }
+//    @AfterEach
+//    void tearDown() {
+//        userRepository.deleteAll();
+//    }
 
     @Test
     public void shouldCreateUserCorrectly() throws UserException {
