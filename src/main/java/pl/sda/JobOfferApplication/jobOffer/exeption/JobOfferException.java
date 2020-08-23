51ec83134d1fe4d048d0f5db7d9ed4da252efe4b
lukaszkomenda <loukas.komenda@gmail.com>
2020-08-23 14:47:05 +0200

    add 008B  exeption and model

---
+++ src/main/java/pl/sda/JobOfferApplication/jobOffer/exeption/JobOfferException.java
@@ -0,0 +1,7 @@
+package pl.sda.JobOfferApplication.jobOffer.exeption;
+
+public class JobOfferException extends Exception {
+    public JobOfferException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/pl/sda/JobOfferApplication/jobOffer/model/Category.java
@@ -0,0 +1,5 @@
+package pl.sda.JobOfferApplication.jobOffer.model;
+
+public enum Category {
+    IT, FoodDrinks, Office, Courier, ShopAssistant;
+}
